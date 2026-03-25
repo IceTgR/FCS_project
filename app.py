245356d58c6af60e787ca35474d48f9ff0bc1e92
Tobias Meier <icetgr@gmail.com>
2026-03-25 18:02:53 +0100

    added comment

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ st.title('F1 Race Strategy Simulator')
 st.write('Hello world!')
 
 st.write(f'This shows that the function from the file feature_01.py ' 
-         f'also works here: 5 squared equals {showcase_square(5)}') # you can test that on streamlit
+         f'also works here once we imported it: 5 squared equals {showcase_square(5)}') # you can test that on streamlit

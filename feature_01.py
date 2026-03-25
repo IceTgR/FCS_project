a1c64b3cf9db9c0d83513107ffa720c8110816f9
Tobias Meier <icetgr@gmail.com>
2026-03-25 16:11:04 +0100

    Added feature_01 file and a function in app.py refering to that

---
+++ feature_01.py
@@ -0,0 +1,5 @@
+# this is just to show that we can use different files to work on different features 
+# (and actually should to avoid merge conflicts)
+
+def showcase_square(number):
+    return number ** 2

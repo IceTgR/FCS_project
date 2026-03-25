6ef165736ceebe571f1485bececf8423a34ea411
Tobias Meier <icetgr@gmail.com>
2026-03-25 16:11:15 +0100

    Changes in app.py

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 import streamlit as st
+from feature_01 import showcase_square #to import function from feature_01.py
 
 st.title('F1 Race Strategy Simulator')
 
 st.write('Hello world!')
+
+st.write(f'This shows that the function from the file feature_01.py also works here: {showcase_square(5)}') # you can test that on streamlit

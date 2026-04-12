e22c7d6aaf6a600652912dd348183a750b17b58a
Tobias Meier <icetgr@gmail.com>
2026-04-12 16:06:57 +0200

    test

--- feature_01.py
+++ feature_01.py
@@ -10,18 +10,22 @@ def write_chosen_options():
     
 def race_simulation():
     st.write(f'Last lap time: {st.session_state.player.lap_time if st.session_state.player.lap > 1 else 'this your first lap'}\n'
-             f'Current lap: {st.session_state.player.lap}')
+            f'Current lap: {st.session_state.player.lap if st.session_state.player.lap <= st.session_state.total_laps else 'finished'}')
     
-    st.session_state.player.calculate_lap_time()
-    
-    if st.button('Stay Out'):
-        st.session_state.player.advance_lap()
-        st.rerun(scope = 'app')
+    if st.session_state.player.lap == st.session_state.total_laps + 1:
+        st.write('Congratulations, you have finished the race!')
 
-    new_tire = st.radio('Choose your tire incase you want to pit', ['soft', 'medium', 'hard'])
-    if st.button('Pit Stop'):
-        st.session_state.player.box(new_tire)
-        st.rerun(scope = 'app')
+    else:
+        st.session_state.player.calculate_lap_time()
+    
+        if st.button('Stay Out'):
+            st.session_state.player.advance_lap()
+            st.rerun(scope = 'app')
+
+        new_tire = st.radio('Choose your tire incase you want to pit', ['soft', 'medium', 'hard'])
+        if st.button('Pit Stop'):
+            st.session_state.player.box(new_tire)
+            st.rerun(scope = 'app')
 
 
 

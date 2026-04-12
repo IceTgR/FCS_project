4ae0289cf18214b846cb9f220eba84c4ab0c0724
Tobias Meier <icetgr@gmail.com>
2026-04-12 13:42:27 +0200

    driver selection

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.write(f'You are now in the seat of a F1 race strategist!\n'
          f' Prepare yourself to make crucial decisions on pit stops, tire choices, and '
          f'guide your driver to victory!')
 
-st.pills('Pit Stop Strategy', 'Tire Management')
+st.radio('Choose your driver:', ['Driver A', 'Driver B', 'Driver C'])
 
 st.write(f'This shows that the function from the file feature_01.py ' 
          f'also works here once we imported it: 5 squared equals {showcase_square(5)}') # you can test that on streamlit

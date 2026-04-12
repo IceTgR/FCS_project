2ed50019fc439b7dbf7f4dbca527a0fdd7438044
Tobias Meier <icetgr@gmail.com>
2026-04-12 16:32:31 +0200

    testingesting

--- feature_01.py
+++ feature_01.py
@@ -11,10 +11,6 @@ def write_chosen_options():
 def race_simulation():
     st.write(f'Last lap time: {st.session_state.player.lap_time if st.session_state.player.lap > 1 else 'this your first lap'}\n'
             f'Current lap: {st.session_state.player.lap if st.session_state.player.lap <= st.session_state.total_laps else 'finished'}')
-    
-    st.write(f'Your last laptimes were: {st.session_state.player.race_history[-1:-6:-1] 
-                                         if len(st.session_state.player.race_history) > 5 
-                                         else st.session_state.player.race_history[::-1]}')
 
     if st.session_state.player.lap == st.session_state.total_laps + 1:
         if st.session_state.player.pitstop_counter == 0:

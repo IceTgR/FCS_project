f6cb50a0a14b153cf1e7936c8350f3d57d0ac079
Tobias Meier <icetgr@gmail.com>
2026-04-12 15:09:01 +0200

    test

--- app.py
+++ app.py
@@ -1,24 +1,36 @@
 import streamlit as st
-from feature_01 import showcase_square #to import function from feature_01.py
+from feature_01 import write_chosen_options
 from car import Car
 from car_monaco import Car_Monaco
 
+if race_started not in st.session_state: # to check if race has started, if not, initialize it to False
+    st.session_state.race_started = False
+
 st.title('F1 Race Strategy Simulator')
 
-st.write(f'You are now in the seat of the F1 race strategist for Ferrari!\n'
+if not st.session_state.race_started:
+    st.write(f'You are now in the seat of the F1 race strategist for Ferrari!\n'
          f' Prepare yourself to make crucial decisions on pit stops, tire choices, and '
          f'guide your driver to victory!')
 
-# User input for driver, track, and starting tire, which is needed to start the simulation
-col1, col2, col3 = st.columns(3)
-driver_player = col1.selectbox('Select your driver:', ['Lewis Hamilton', 'Charles Leclerc'])
 
-Track = col2.selectbox('Select the track:', ['Monaco', 'Silverstone'])
 
-tire_start = col3.radio('Choose your starting tire:', ['soft', 'medium', 'hard'])
+    # User input for driver, track, and starting tire, which is needed to start the simulation
+    col1, col2, col3 = st.columns(3)
+    driver_player = col1.selectbox('Select your driver:', ['Lewis Hamilton', 'Charles Leclerc'])
 
-if not st.session_state.race_started:
-    st.session_state.race_started = True
-    st.session_state.player = Car(driver_player, Track, tire_start) # create an instance
-    st.rerun
+    track = col2.selectbox('Select the track:', ['Monaco', 'Silverstone'])
+
+    tire_start = col3.radio('Choose your starting tire:', ['soft', 'medium', 'hard'])
+
+    if st.button('Start the simulation'):
+        st.session_state.race_started = True
+        if track == 'Monaco':
+            st.session_state.player = Car_Monaco(driver_player, tire_start) # create an instance for Monaco
+        if track == 'Silverstone':
+            st.session_state.player = Car(driver_player, tire_start) # create an instance for Silverstone
+        st.rerun
+
+if st.session_state.race_started:
+    write_chosen_options()
 

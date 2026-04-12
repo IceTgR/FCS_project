fc8ba42e44d31eb41110c0b4d66b18a471a5bf80
Tobias Meier <icetgr@gmail.com>
2026-04-12 17:06:38 +0200

    test

--- feature_01.py
+++ feature_01.py
@@ -33,11 +33,13 @@ def race_simulation():
 
     st.subheader('Race History')
     history = pd.DataFrame(st.session_state.player.race_history, columns=['Lap', 'Lap Time', 'Tire', 'Tire Age', 'Pitstop'])
-    st.table(history)
-
-    st.metric('Lap Time', f"{st.session_state.player.race_history[-1]['Lap Time']:.2f} seconds",
+    st.metric('Lap Time Development', f"{st.session_state.player.race_history[-1]['Lap Time']:.2f} seconds",
             delta = (st.session_state.player.race_history[-2]['Lap Time'] - st.session_state.player.race_history[-1]['Lap Time'] if st.session_state.player.lap > 2 else 0),
             chart_data = history['Lap Time'], chart_type='line', border = True)
+    
+    st.table(history)
+
+
     
 
 

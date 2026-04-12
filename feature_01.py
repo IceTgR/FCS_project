563e611c5a81d75737a71859e78be2844d89734b
Tobias Meier <icetgr@gmail.com>
2026-04-12 15:12:03 +0200

    debug

--- feature_01.py
+++ feature_01.py
@@ -3,7 +3,7 @@ import streamlit as st
 
 def write_chosen_options():
     st.write(f'You have selected {st.session_state.player.driver} as your driver, '
-        f'starting on {st.session_state.player.tire} tires at the {track} track.')
+        f'starting on {st.session_state.player.tire} tires at the {st.session_state.track} track.')
 
 
 
